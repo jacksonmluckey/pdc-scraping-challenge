6a7c5231967fbc24bd22271906f436db7d4ce1a4
Jackson Luckey <jacksonmluckey@gmail.com>
2024-04-07 14:27:57 +0200

    finished

--- ctf.py
+++ ctf.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 root_url = 'https://hertie-scraping-website.vercel.app/'
 
@@ -28,5 +31,63 @@ for flag in s.find_all('div', class_=flag_text):
 for flag in s.find_all('div', id = flag_text):
     flags.add(flag['id'])
 
-for flag in flags:
+
+page2 = "wowimlevel2"
+req = requests.get(root_url + page2)
+s = BeautifulSoup(req.content, 'html.parser')
+
+# Get the 41st flag
+flags.add(re.findall('flag-[0-9]{2}', s.find('div', class_='my-4').get_text())[0])
+
+# Get the flags stored in divs that start with "flag" (div)
+for flag in s.find_all('div', class_=flag_text):
+    flags.add(flag['class'][0])
+
+# Get the flags stored in divs that start with "flag" (class)
+for flag in s.find_all('div', id = flag_text):
+    flags.add(flag['id'])
+
+# Some of the remaing flags live inside scripts
+for script in s.find_all('script'):
+    try:
+        for flag in re.findall('flag-[0-9]+', script.get_text()):
+            flags.add(flag)
+    except:
+        pass
+
+page3 = "goodjobfindinglevel3"
+req = requests.get(root_url + page3)
+s = BeautifulSoup(req.content, 'html.parser')
+
+for button in s.find_all('button', id = flag_text):
+    flags.add(button['id'])
+
+options = webdriver.FirefoxOptions()
+options.binary_location = "~/Downloads/geckodriver"
+driver = webdriver.Firefox()
+driver.get(root_url + page3)
+buttons = driver.find_elements(By.XPATH, "//button")
+for button in buttons:
+    try:
+        button.click()
+    except:
+        pass
+buttons = driver.find_elements(By.XPATH, "//button")
+for button in buttons:
+    flags.add(button.text)
+
+page4 = "finalboss"
+driver.get(root_url + page4)
+input_element = driver.find_element(By.CSS_SELECTOR, 'input[type="text"]')
+input_element.clear()
+input_element.send_keys("!!flag-61!!")
+divs = driver.find_elements(By.XPATH, "//div")
+print(divs)
+for div in divs:
+    for flag in re.findall('flag-[0-9]+', div.text):
+        flags.add(flag)
+
+
+sorted_flags = sorted(flags, key=lambda x: int(x.split('-')[1]))
+for flag in sorted_flags:
     print(flag)
